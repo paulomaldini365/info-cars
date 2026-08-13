358f0c8fdeef15bfcdf1a70622ed4f2b099aa82b
paulomaldini <maldin.buci@gmail.com>
2026-08-13 23:58:27 +0200

    Fix syntax error in PORT environment variable retrieval

--- app.py
+++ app.py
@@ -278,5 +278,5 @@ def brand_page(brand_name):
     return render_template('brand.html', brand_name=brand_name, cars=cars)
 
 if __name__ == '__main__':
-    port = int(os.environ.get('PORT' 5000))
+    port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port)
