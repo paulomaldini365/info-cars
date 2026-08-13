c1128d776d023b2f960cdda4577a9c17992b0ca6
paulomaldini <maldin.buci@gmail.com>
2026-08-13 23:51:50 +0200

    Update Flask app to use environment variable for port

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request
 
 app = Flask(__name__)
@@ -277,4 +278,5 @@ def brand_page(brand_name):
     return render_template('brand.html', brand_name=brand_name, cars=cars)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    port = int(os.environ.get('PORT' 5000))
+    app.run(host='0.0.0.0', port=port)
